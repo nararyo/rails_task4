98a816a2da37b40a4d9566fde6d3419ddccea6d3
奈良亮 <motorbicykle@gmail.com>
2020-03-16 00:00:05 +0900

    テキスト教材has manyの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'items/index'
   root to: 'items#index'
-  resources :items
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items
+
+  resources :users, only: :show
+
+
 end
